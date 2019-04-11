84cdd0eda90d8ef0718ebb9a301faf60e7e147a3
Yixuan He <s1891035@ed.ac.uk>
2019-04-11 17:32:04 +0100

    data and all that

---
+++ data/toy examples/dilation.py
@@ -0,0 +1,35 @@
+import numpy as np
+import cv2
+import os
+
+INPUT_PATH = 'F:/fill_boundary/input_images0320/'
+OUTPUT_PATH_FILL = 'F:/fill_boundary/input_images0320/'
+colorThreshold = 30
+
+def dilation(inputPath, savePath, boundaryColor, d):
+    #print(inputPath)
+    img = cv2.imread(inputPath, 0)
+    height, width = img.shape[:2]
+    result = np.zeros([height * d, width * d])
+    for x in range(height):
+        for y in range(width):
+            if abs(img[x,y] - boundaryColor) < colorThreshold:
+                #fill the rectangle
+                for i in range(d*x, d*(x+1)):
+                    for j in range(d*y, d*(y+1)):
+                        result[i,j] = boundaryColor
+    cv2.imwrite(savePath, result, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
+
+def dilation_main():
+    boundaryColor = 255
+    for d, n in enumerate(range(200, 2200, 200)):
+        output_folder_name = 'input_' + str(n)
+        input_folder_name = 'input_' + str(200)
+        input_path = os.path.join(INPUT_PATH, input_folder_name)
+        output_path_fill = os.path.join(OUTPUT_PATH_FILL, output_folder_name)
+        for root, dirs, files in os.walk(input_path):
+            for f in files:
+                dilation(os.path.join(input_path, f), os.path.join(output_path_fill, f), boundaryColor, d+1)
+
+if __name__ == '__main__':
+    dilation_main()
